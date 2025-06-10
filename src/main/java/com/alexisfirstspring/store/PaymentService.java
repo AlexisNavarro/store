98af38f0a95f7e48c38329b5c40df6be2ef9809c
Alexis Navarro <navarroalexis007@gmail.com>
2025-06-10 00:11:57 -0600

    created a payment service interface to allow flexbility on what type of payment will be allowed

--- src/main/java/com/alexisfirstspring/store/PaymentService.java
+++ src/main/java/com/alexisfirstspring/store/PaymentService.java
@@ -1,4 +1,6 @@
 package com.alexisfirstspring.store;
 
+//Interface for payments done which will be called upon Stripe or PayPal
 public interface PaymentService {
+    void processPayment(double amount);
 }
