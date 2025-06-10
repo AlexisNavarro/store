4948af85b2617dfddfcf17785e9bdd29a72d9e7e
Alexis Navarro <navarroalexis007@gmail.com>
2025-06-10 00:15:28 -0600

    made a file to be able to perform Orders such as the amount we want to pay and the payment method we want to use

--- src/main/java/com/alexisfirstspring/store/OrderService.java
+++ src/main/java/com/alexisfirstspring/store/OrderService.java
@@ -1,4 +1,19 @@
 package com.alexisfirstspring.store;
 
+import java.time.temporal.ValueRange;
+
 public class OrderService {
+
+    //this references any class that implements PaymentService
+    private PaymentService paymentService;
+
+    //constructor to pass in the specific payment method we want to use
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    //method that places an order and processes a payment of $10
+    public void placeOrder(){
+        paymentService.processPayment(10);
+    }
 }
