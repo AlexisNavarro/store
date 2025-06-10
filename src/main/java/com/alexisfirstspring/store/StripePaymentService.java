31ff01f84f49d7ba448bb333ee689e59cfa748d9
Alexis Navarro <navarroalexis007@gmail.com>
2025-06-10 00:11:21 -0600

    made a Stripe payment service that will reference PaymentService interface

--- src/main/java/com/alexisfirstspring/store/StripePaymentService.java
+++ src/main/java/com/alexisfirstspring/store/StripePaymentService.java
@@ -1,4 +1,13 @@
 package com.alexisfirstspring.store;
 
-public class StripePaymentService {
+
+//This class will now reference the payment service class
+//This class can be used interchangeably with any other payment service
+//allowing for flexibility and adherence to open/closed principle
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount){
+        System.out.println("STRIPE");
+        System.out.println("Amount: " + amount);
+    }
 }
