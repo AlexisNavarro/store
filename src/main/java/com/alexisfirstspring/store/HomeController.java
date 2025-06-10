50ca1d0a2ced1608010f178ba8c46db33958f449
Alexis Navarro <navarroalexis007@gmail.com>
2025-06-09 20:23:01 -0600

    checking if the page size was modified through a print statement in the method

--- src/main/java/com/alexisfirstspring/store/HomeController.java
+++ src/main/java/com/alexisfirstspring/store/HomeController.java
@@ -1,14 +1,19 @@
 package com.alexisfirstspring.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
 
     //request mapping "/" points to the root of the website
     @RequestMapping("/")
     public String index(){
+        System.out.println("appName: " + appName);
         return "index.html";
     }
+
 }
